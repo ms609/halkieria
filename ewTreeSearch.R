2753894c1979e2886a5eeb4800b028f393f7a51e
Martin Smith <martins@gmail.com>
2019-09-18 15:58:04 +0100

    search scripts

---
+++ ewTreeSearch.R
@@ -0,0 +1,24 @@
+library('TreeSearch')
+# Load data from locally downloaded copy of MorphoBank matrix
+nexusFile <- MorphoBank::MostRecentNexus()
+my_data <- ReadAsPhyDat(nexusFile)
+ignored_taxa <- c('Conotheca', 'Maxilites', 'Pauxillites',
+                  'Probactrotheca') # Also manually update tnt.run using `taxcode-`
+my_data[ignored_taxa] <- NULL
+iw_data <- PrepareDataIW(my_data)
+outgroup <- 'Loxosomella'
+
+nj.tree <- NJTree(my_data)
+rooted.tree <- EnforceOutgroup(nj.tree, outgroup)
+start.tree <- TreeSearch(tree=rooted.tree, dataset=my_data, maxIter=3000,
+                         EdgeSwapper=RootedNNISwap, verbosity=0)
+
+ew.tree <- Ratchet(start.tree, my_data, verbosity=3L,
+                   ratchHits = 20, ratchIter=4000, searchHits=55,
+                   swappers=list(RootedTBRSwap, RootedSPRSwap, RootedNNISwap))
+ew.consensus <- RatchetConsensus(ew.tree, my_data, nSearch=250, searchHits = 85,
+                                 swappers=list(RootedTBRSwap, RootedNNISwap),
+                                 verbosity=2L)
+
+ape::write.nexus(ew.consensus, file=paste0(collapse='', "TreeSearch/hk_ew_",
+                                      Fitch(ew.tree, my_data), ".nex"))
---
+++ treeSearch10.5.R
@@ -0,0 +1,34 @@
+library('TreeSearch')
+# Load data from locally downloaded copy of MorphoBank matrix
+nexusFile <- MorphoBank::MostRecentNexus()
+my_data <- ReadAsPhyDat(nexusFile)
+ignored_taxa <- c('Conotheca', 'Maxilites', 'Pauxillites',
+                  'Probactrotheca') # Also manually update tnt.run using `taxcode-`
+my_data[ignored_taxa] <- NULL
+iw_data <- PrepareDataIW(my_data)
+outgroup <- 'Loxosomella'
+
+nj.tree <- NJTree(my_data)
+rooted.tree <- EnforceOutgroup(nj.tree, outgroup)
+start.tree <- TreeSearch(tree=rooted.tree, dataset=my_data, maxIter=3000,
+                         EdgeSwapper=RootedNNISwap, verbosity=0)
+
+k <- 10.5
+iw.tree <- IWRatchet(start.tree, iw_data, concavity=k,
+                     ratchHits = 20, ratchIter=4000, searchHits=56,
+                     swappers=list(RootedTBRSwap, RootedSPRSwap, RootedNNISwap),
+                     verbosity=3L)
+score <- IWScore(iw.tree, iw_data, concavity=k)
+# Write a single best tree
+ape::write.nexus(iw.tree,
+                 file=paste0("TreeSearch/hk_iw_k", k, "_",
+                             signif(score, 5), ".nex", collapse=''))
+
+iw.consensus <- IWRatchetConsensus(iw.tree, iw_data, concavity=k,
+                                   swappers=list(RootedTBRSwap, RootedNNISwap),
+                                   searchHits=55, searchIter=4000, nSearch=250,
+                                   verbosity=3L)
+ape::write.nexus(iw.consensus,
+                 file=paste0("TreeSearch/hk_iw_k", k, "_",
+                             signif(IWScore(iw.consensus[[1]], iw_data, concavity=k), 5),
+                             ".all.nex", collapse=''))
