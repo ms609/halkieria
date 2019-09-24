3be51088d7d03a68750a8fe500b783c5f63fb003
Martin Smith <martins@gmail.com>
2019-09-24 16:02:50 +0100

    Update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -4,8 +4,8 @@ set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-git config --global user.email "martin.smith@durham.ac.uk"
-git config --global user.name "Martin Smith"
+git config --global user.email "martins@gmail.com"
+git config --global user.name "Martin Smith via Travis"
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
