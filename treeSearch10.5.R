c995ebb52196d9e90f89b64ee279a6611b27e522
Martin Smith <martins@gmail.com>
2019-09-26 10:29:36 +0100

    Embolden and tidy analyses

--- treeSearch10.5.R
+++ treeSearch10.5.R
@@ -2,23 +2,23 @@ library('TreeSearch')
 # Load data from locally downloaded copy of MorphoBank matrix
 nexusFile <- MorphoBank::MostRecentNexus()
 my_data <- ReadAsPhyDat(nexusFile)
-ignored_taxa <- c('Conotheca', 'Maxilites', 'Pauxillites',
-                  'Probactrotheca') # Also manually update tnt.run using `taxcode-`
+ignored_taxa <- c('Conotheca', 'Maxilites', 'Pauxillites', 'Probactrotheca')
 my_data[ignored_taxa] <- NULL
 iw_data <- PrepareDataIW(my_data)
 outgroup <- 'Yilingia_spiciformis'
 
 nj.tree <- NJTree(my_data)
 rooted.tree <- EnforceOutgroup(nj.tree, outgroup)
-start.tree <- TreeSearch(tree=rooted.tree, dataset=my_data, maxIter = 3000,
+start.tree <- TreeSearch(tree=rooted.tree, dataset=my_data, maxIter = 20000,
                          EdgeSwapper=RootedNNISwap, verbosity=0)
 
 k <- 10.5
 iw.tree <- IWRatchet(start.tree, iw_data, concavity=k,
-                     ratchHits = 20, ratchIter = 4000, searchHits = 56,
+                     ratchHits = 24, ratchIter = 4000,
+                     searchHits = 64, searchiter = 4000,
                      swappers=list(RootedTBRSwap, RootedSPRSwap, RootedNNISwap),
                      verbosity=4L)
-score <- IWScore(iw.tree, iw_data, concavity=k)
+score <- IWScore(iw.tree, iw_data, concavity = k)
 # Write a single best tree
 ape::write.nexus(iw.tree,
                  file=paste0("TreeSearch/hk_iw_k", k, "_",
@@ -26,7 +26,7 @@ ape::write.nexus(iw.tree,
 
 iw.consensus <- IWRatchetConsensus(iw.tree, iw_data, concavity = k,
                                    swappers = list(RootedTBRSwap, RootedNNISwap),
-                                   searchHits = 55, searchIter = 4000,
+                                   searchHits = 55, searchIter = 8000,
                                    nSearch = 250, verbosity = 4L)
 ape::write.nexus(
   iw.consensus,
